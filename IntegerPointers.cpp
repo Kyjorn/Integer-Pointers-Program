08c96f701e47c057b3f925c15bf71e5012fa3335
Kyjron <hendonmshane@gmail.com>
2025-04-07 10:17:59 -0600

    Remove VSCode configuration files and update IntegerPointers.cpp to display user input.

--- IntegerPointers.cpp
+++ IntegerPointers.cpp
@@ -9,6 +9,12 @@ int main() {
     int num2;
     int num3;
     
+    //user input
+    cout << "Enter three numbers followed by a space: ";
+    cin >> num1 >> num2 >> num3;
+
+    //test display
+    //cout << num1 << " " << num2 << " " << num3;
     
 return 0;
 }
